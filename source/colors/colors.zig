ea61f00394fefe0b60fc541f0ceedf9fc62866ff
Renan Lucas <renanluscad@gmail.com>
2025-10-26 15:33:10 -0300

    Delete source directory

--- source/colors/colors.zig
+++
@@ -1,7 +0,0 @@
-pub const colors: type = struct {
-    pub const reset: *const [4:0]u8 = "\x1b[0m";
-    pub const white: *const [5:0]u8 = "\x1b[37m";
-    pub const green: *const [5:0]u8 = "\x1b[32m";
-    pub const red: *const [5:0]u8 = "\x1b[31m";
-    pub const bold: *const [4:0]u8 = "\x1b[1m";
-};
